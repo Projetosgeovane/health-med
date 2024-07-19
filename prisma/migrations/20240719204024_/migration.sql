edfe3c92bf9d1e232d2d4362e86fa62cb2548431
Geovane Silva <geovanesilva.nar@hotmail.com>
2024-07-19 18:19:22 -0300

    feat: add Prisma migration for creating Availability, Appointment, and MedicalRecord tables

---
+++ prisma/migrations/20240719204024_/migration.sql
@@ -0,0 +1,42 @@
+-- CreateTable
+CREATE TABLE `Availability` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `date` VARCHAR(191) NOT NULL,
+    `time` VARCHAR(191) NOT NULL,
+    `userId` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Appointment` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `date` VARCHAR(191) NOT NULL,
+    `time` VARCHAR(191) NOT NULL,
+    `status` VARCHAR(191) NOT NULL,
+    `doctorId` INTEGER NOT NULL,
+    `patientId` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `MedicalRecord` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `document` VARCHAR(191) NOT NULL,
+    `patientId` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Availability` ADD CONSTRAINT `Availability_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Appointment` ADD CONSTRAINT `Appointment_doctorId_fkey` FOREIGN KEY (`doctorId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Appointment` ADD CONSTRAINT `Appointment_patientId_fkey` FOREIGN KEY (`patientId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `MedicalRecord` ADD CONSTRAINT `MedicalRecord_patientId_fkey` FOREIGN KEY (`patientId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
