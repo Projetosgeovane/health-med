d13f7b0773203c41a1446d8918f01ae5923ac4e2
Geovane Silva <geovanesilva.nar@hotmail.com>
2024-07-22 14:41:52 -0300

    feat: Update CreateUserController import in HttpModule

--- src/modules/appointment/domain/infra/http/http.module.ts
+++ src/modules/appointment/domain/infra/http/http.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { UserPersistenceModule } from '../persistence/persistence.module';
 import { CreateUserUseCase } from '../../application/use-cases/appointment/create-user.use-case';
-import { CreateUserController } from './controllers/appointment/create-user.controller';
+import { CreateUserController } from './controllers/appointment/create-appointment.controller';
 
 @Module({
   imports: [UserPersistenceModule],
