b7b974a056144d7f53ece3fcff0b0433fbb78ac8
Geovane Silva <geovanesilva.nar@hotmail.com>
2024-07-22 14:48:47 -0300

    feat: Remove user-related code

--- src/modules/appointment/domain/application/repositories/user.repository.ts
+++
@@ -1,4 +0,0 @@
-import { BaseRepository } from 'libs/core/src/repositories';
-import { UserEntity } from '../../enterprise/users.entity';
-
-export abstract class UserRepository extends BaseRepository<UserEntity> { }
