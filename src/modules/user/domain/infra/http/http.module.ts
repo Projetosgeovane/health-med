cee440231015574cb337465b88dd81f4b5cfd0fc
Geovane Silva <geovanesilva.nar@hotmail.com>
2024-07-22 14:24:49 -0300

    feat: Implement EditUserUseCase for editing user information

--- src/modules/user/domain/infra/http/http.module.ts
+++ src/modules/user/domain/infra/http/http.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { PersistenceModule } from '../persistence/persistence.module';
 import { CreateUserUseCase } from '../../application/use-cases/user/create-user.use-case';
 import { CreateUserController } from './controllers/user/create-user.controller';
+import { CreateAvailabilityUseCase } from '../../application/use-cases/availability/create-availability.use-case';
+import { CreateAvailabilityController } from './controllers/availability/create-availability.controller';
 
 @Module({
   imports: [PersistenceModule],
-  controllers: [CreateUserController],
-  providers: [CreateUserUseCase],
+  controllers: [CreateUserController, CreateAvailabilityController],
+  providers: [CreateUserUseCase, CreateAvailabilityUseCase],
 })
 export class HttpModule { }
--- src/modules/user/domain/infra/persistence/persistence.module.ts
+++ src/modules/user/domain/infra/persistence/persistence.module.ts
@@ -2,19 +2,31 @@ import { Module } from '@nestjs/common';
 import { DatabaseModule } from 'src/common/database/database.module';
 import { UserRepository } from '../../application/repositories/user.repository';
 import { PrismaUserRepositoryImpl } from './prisma/user/repositories/prisma-user-repository.impl';
+import { PrismaAvailabilityRepositoryImpl } from './prisma/availability/repositories/prisma-availability-repository.impl';
+import { AvailabilityRepository } from '../../application/repositories/availability.repository';
 
 @Module({
   imports: [DatabaseModule],
   providers: [
     // --
     PrismaUserRepositoryImpl,
+    PrismaAvailabilityRepositoryImpl,
     // --
 
     {
       useClass: PrismaUserRepositoryImpl,
       provide: UserRepository,
     },
+    {
+      useClass: PrismaAvailabilityRepositoryImpl,
+      provide: AvailabilityRepository,
+    },
   ],
-  exports: [PrismaUserRepositoryImpl, UserRepository],
+  exports: [
+    PrismaUserRepositoryImpl,
+    UserRepository,
+    PrismaAvailabilityRepositoryImpl,
+    AvailabilityRepository,
+  ],
 })
 export class PersistenceModule { }
