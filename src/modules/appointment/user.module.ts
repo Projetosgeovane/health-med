4f11f8840b98c7e6a13790d378c9d070fe3b2cf0
Geovane Silva <geovanesilva.nar@hotmail.com>
2024-07-22 14:56:42 -0300

    feat: Remove unused user-related code

--- src/modules/appointment/user.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { UserHttpModule } from './domain/infra/http/http.module';
-import { UserPersistenceModule } from './domain/infra/persistence/persistence.module';
-@Module({
-  imports: [UserHttpModule, UserPersistenceModule],
-})
-export class UserModule { }
