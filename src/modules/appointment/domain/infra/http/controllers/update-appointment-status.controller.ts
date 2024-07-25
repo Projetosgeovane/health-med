3fe097f423a50ebba1dff5df2a9c2de1fcee56ee
Geovane Silva <geovanesilva.nar@hotmail.com>
2024-07-25 20:45:13 -0300

    feat: Update appointment status endpoint to accept status in request body

--- src/modules/appointment/domain/infra/http/controllers/update-appointment-status.controller.ts
+++ src/modules/appointment/domain/infra/http/controllers/update-appointment-status.controller.ts
@@ -1,24 +1,31 @@
 import { ResourceNotFoundError } from '@enablers/core/errors';
 import {
   BadRequestException,
+  Body,
   Controller,
   HttpCode,
   NotFoundException,
   Param,
   Patch,
 } from '@nestjs/common';
+import { UpdateAppointmentStatusUseCase } from '../../../application/use-cases/appointment/update-appointment-status.use-case';
 
-@Controller('device/parser-enabled')
+@Controller()
 export class UpdateAppointmentStatusController {
   constructor(
     private readonly updateAppointmentStatusUseCase: UpdateAppointmentStatusUseCase,
   ) { }
 
-  @Patch(':id')
+  @Patch('appointments/:id/status')
   @HttpCode(204)
-  async handle(@Param('id') appointmentID: string) {
+  async handle(
+    @Param('id') appointmentID: string,
+    @Body() body: { status: string },
+  ) {
+    const { status } = body;
     const result = await this.updateAppointmentStatusUseCase.execute({
       appointmentID,
+      status,
     });
 
     if (result.isFailure()) {
